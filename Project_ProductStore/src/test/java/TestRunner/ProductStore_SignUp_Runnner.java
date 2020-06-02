34cadf1775ffbad8ac9a8e9c597fa2bb10300563
sushmitha4mail <62542708+sushmitha4mail@users.noreply.github.com>
2020-06-02 13:37:27 +0530

    commit

---
+++ Project_ProductStore/src/test/java/TestRunner/ProductStore_SignUp_Runnner.java
@@ -0,0 +1,17 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = ".\\src\\main\\resources\\feature\\test.feature",
+    plugin = { "pretty","html:reports/cucumber-html-report" ,"json:reports/cucumber-html-report/jsonreport",
+    "com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"}, 
+    tags = {"@TC02_Project_ProductStore,@TC03_Project_ProductStore,@TC04_Project_ProductStore,@TC05_Project_ProductStore" },
+    glue = {"com.testCases" }, 
+    monochrome = true)
+public class ProductStore_SignUp_Runnner {
+
+}
